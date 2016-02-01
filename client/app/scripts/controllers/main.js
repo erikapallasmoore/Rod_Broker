6db2ce8e2baf281305642710a8b037edf8d824e7
raichel81 <rai.chel.ann@gmail.com>
2016-01-31 16:39:44 -0800

    set up scaffolding

---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rodBrokerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rodBrokerApp
+ */
+angular.module('rodBrokerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
