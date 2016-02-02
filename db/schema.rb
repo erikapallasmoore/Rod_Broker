2b299cf55aae97887ffbd27a86fd4d35db4de371
raichel81 <rai.chel.ann@gmail.com>
2016-02-01 21:16:50 -0800

    Update schema files post-migration

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,12 @@
 ActiveRecord::Schema.define(version: 20160202024551) do
 
   create_table "groups", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
+
+  add_index "groups", ["name"], name: "index_groups_on_name", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
