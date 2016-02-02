25fb6e50daa618351d50df36b15a4872b71de87a
raichel81 <rai.chel.ann@gmail.com>
2016-02-01 17:47:18 -0800

    ruby and angular scaffolding connected

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -8,33 +8,36 @@
  *
  * Main module of the application.
  */
-angular
-  .module('rodBrokerApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+var app = angular.module('rodBrokerApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch'
+])
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'AboutCtrl'
+    })
+    .when('/groups', {
+      templateUrl: 'views/groups.html',
+      controller: 'GroupsCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
+ 
+app.factory('Group', ['$resource', function($resource) {
+  return $resource('/api/groups/:id.json', null, {
+    'update': { method:'PUT' }
   });
+}]);
